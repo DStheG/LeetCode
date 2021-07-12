6f22cfad57509affeb8c54ae680fcbc0508c09f8
Han, Dongseok <xcrossx@gmail.com>
2021-07-12 23:46:46 +0900

    Refine

--- add-two-numbers/add-two-numbers.py
+++ add-two-numbers/add-two-numbers.py
@@ -5,22 +5,29 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        v = l1.val + l2.val
         c = 0
+        if v > 9: 
+            c = 1
+            v -= 10
+        prev = ret = ListNode(v)
+        l1 = l1.next
+        l2 = l2.next
         
-        ret = None
         while l1 or l2:
             v1 = 0 if l1 == None else l1.val
             v2 = 0 if l2 == None else l2.val
+
             v = v1 + v2 + c
-            n = ListNode(v % 10)
-            if (ret == None):
-                ret = n
-                prev = n
-            else:
-                prev.next = n
-                prev = n
-                
-            c = v // 10
+
+            c = 0
+            if v > 9: 
+                c = 1
+                v -= 10
+
+            prev.next = ListNode(v)
+            prev = prev.next  
+            
             if l1:
                 l1 = l1.next
             if l2:
