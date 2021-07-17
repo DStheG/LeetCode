83187b6d7d4225f29f610dc7a9ac65b3a71cbcf7
Han, Dongseok <xcrossx@gmail.com>
2021-07-18 00:25:55 +0900

    using list and set

--- partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,10 +1,5 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        m = -1
-        for i in n:
-            print(i)
-            if int(i) > m:
-                m = int(i)
-            if m == 9:
-                return m
-        return m
+        l = [int(i) for i in n]
+        s = set(l)
+        return max(s)
